e5eecae090491de2cf7f4b55a227ac6165b46032
sparsh-anurag <78465897+sparsh-anurag@users.noreply.github.com>
2022-11-11 14:20:59 +0530

    Delete script.sh

--- example/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-aws kms encrypt \
-  --key-id 0e277743-6827-4507-94cc-b58def888f3a \
-  --region ap-south-1 \
-  --plaintext fileb://document-db-creds-non-prod.yml \
-  --output text \
-  --query CiphertextBlob --profile non-prod-eurokids \
-  > document-db-creds-non-prod.yml.encrypted
